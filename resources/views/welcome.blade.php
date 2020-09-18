7bd5cbe4b24a2a09f3331717d3143488de388477
mprince2k18 <mprince2k16@gmail.com>
2020-09-19 01:14:41 +0600

    silder update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
     <div class=" mb-3">
         <h1 class="font-17"><a href="javascript:void()" class="px-3 color-theme">Blogs</a></h1>
     </div>
-    <div class="double-slider owl-carousel owl-no-dots mb-4">
+    <div class="single-slider owl-carousel owl-no-dots mb-4">
 
         {{-- TODO:: BLOG LOOP --}}
         @foreach ($blogs as $blog)
